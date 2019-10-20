3d6c67d78b5a0e0ed182026d8f30b84ce1ea10b5
Antti Kivimäki <antti.kivimaki@futurice.com>
2019-10-20 16:39:12 +0300

    Fix tests

--- test/telldus-core.test.ts
+++ test/telldus-core.test.ts
@@ -1,12 +1,12 @@
 import 'mocha';
 import { expect } from 'chai';
-import TellstickClient from '../src/client';
+import TellstickCoreClient from '../src/clients/telldus-core';
 
 describe('TellstickClient', () => {
   describe('constructor', () => {
     it('parses file paths correctly', () => {
       const path = '/tmp/Tellstick';
-      const core = new TellstickClient(path);
+      const core = new TellstickCoreClient(path);
 
       expect(core.connectionOptions).to.eql({ path });
     });
@@ -14,7 +14,7 @@ describe('TellstickClient', () => {
     it('parses host and port correctly', () => {
       const host = '192.168.1.10';
       const port = 5000;
-      const core = new TellstickClient(`${host}:${port}`);
+      const core = new TellstickCoreClient(`${host}:${port}`);
 
       expect(core.connectionOptions).to.eql({ host, port });
     });
