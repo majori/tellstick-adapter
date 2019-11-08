e8fb851157a25cb740acac6993f887caa1fe9563
Antti Kivimäki <antti.kivimaki@futurice.com>
2019-11-08 21:16:31 +0200

    Housecleaning

--- test/adapter.test.ts
+++ test/adapter.test.ts
@@ -1,7 +1,6 @@
 import 'mocha';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import TellstickAdapter from '../src/adapter';
 import { TelldusCoreClient, LocalAPIClient } from '../src/clients';
 import * as utils from './utils';
 
@@ -10,16 +9,12 @@ describe('Adapter', () => {
     sinon.restore();
   });
 
-  it('adds adapter to the manager', () => {
-    const manager = utils.createAddonManager();
-    const manifest = utils.createManifest({ client: 'local-api' });
-    const adapter = new TellstickAdapter(manager, manifest);
-
-    expect(manager.addAdapter.calledWith(adapter)).to.be.true;
-  });
-
   it('selects "local-api" client based on the config', () => {
-    const adapter = utils.createAdapter({ client: 'local-api' });
+    const adapter = utils.createAdapter({
+      client: 'local-api',
+      url: 'http://192.168.1.100',
+      token: 'test_token',
+    });
 
     expect(adapter.client).to.be.an.instanceOf(LocalAPIClient);
   });
--- test/clients/telldus-core.test.ts
+++ test/clients/telldus-core.test.ts
@@ -6,17 +6,17 @@ describe('telldus-core', () => {
   describe('constructor', () => {
     it('parses file paths correctly', () => {
       const socket = '/tmp/Tellstick';
-      const core = new TelldusCoreClient({ socket });
+      const client = new TelldusCoreClient({ socket });
 
-      expect(core.connectionOptions).to.eql({ socket });
+      expect(client.connectionOptions).to.eql({ socket });
     });
 
     it('parses host and port correctly', () => {
       const host = '192.168.1.10';
       const port = 5000;
-      const core = new TelldusCoreClient({ socket: `${host}:${port}` });
+      const client = new TelldusCoreClient({ socket: `${host}:${port}` });
 
-      expect(core.connectionOptions).to.eql({ host, port });
+      expect(client.connectionOptions).to.eql({ host, port });
     });
   });
 });
